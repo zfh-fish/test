dac5854ec11f4f1490570cbf22ff6be3507da423
前进的小学生 <zfh_fish@163.com>
2019-10-22 08:21:51 +0800

    123

---
+++ src/main/java/com/fish/SpringbootChulinApplication.java
@@ -0,0 +1,13 @@
+package com.fish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootChulinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootChulinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fish/SpringbootChulinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootChulinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
